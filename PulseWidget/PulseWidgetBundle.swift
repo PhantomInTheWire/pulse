03c693df5f37e263616a3b70db1ab71824bda787
Karan <karanlokchandani@protonmail.com>
2025-11-12 00:42:44 +0530

    chore: init basic
    Signed-off-by: Karan <karanlokchandani@protonmail.com>

---
+++ PulseWidget/PulseWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  PulseWidgetBundle.swift
+//  PulseWidget
+//
+//  Created by Karan Haresh Lokchandani on 12/11/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct PulseWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        PulseWidget()
+    }
+}
