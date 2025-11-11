4d4b7bbc2b594bd9d7aac732a13ec7d63a8869ce
Karan <karanlokchandani@protonmail.com>
2025-11-12 00:28:51 +0530

    chore: init
    Signed-off-by: Karan <karanlokchandani@protonmail.com>

---
+++ Pulse/PulseApp.swift
@@ -0,0 +1,17 @@
+//
+//  PulseApp.swift
+//  Pulse
+//
+//  Created by Karan Haresh Lokchandani on 12/11/25.
+//
+
+import SwiftUI
+
+@main
+struct PulseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
